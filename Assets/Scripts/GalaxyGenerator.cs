bea3b8b969f57595539544b63a160e5dd6a3d6f6
asampley <sampley@ualberta.ca>
2018-03-17 12:04:17 -0600

    Made galaxy generator script produce planets that do not collide, and are spawned in equal bands

--- Assets/Scripts/GalaxyGenerator.cs
+++ Assets/Scripts/GalaxyGenerator.cs
@@ -8,6 +8,8 @@ public class GalaxyGenerator : MonoBehaviour {
 	public int numPlanets;
 	public float minDistance;
 	public float maxDistance;
+	public float minPlanetRadius;
+	public float maxPlanetRadius;
 
 	public int seed = 0x12345678;
 	System.Random rand;
@@ -20,11 +22,34 @@ public class GalaxyGenerator : MonoBehaviour {
 		GameObject sun = Instantiate (sunPrefab, this.transform);
 
 		for (int i = 0; i < numPlanets; ++i) {
-			Orbiter orbiter = Instantiate (planetPrefab, sun.transform).GetComponent<Orbiter>();
-			orbiter.center = sun.transform;
-			orbiter.ccw = rand.Next () % 2 == 0;
-			orbiter.distance = (float)(rand.NextDouble () * (maxDistance - minDistance) + minDistance);
-			orbiter.period = orbiter.distance;
+			float minD = (i + 0) * (maxDistance - minDistance) / numPlanets + minDistance;
+			float maxD = (i + 1) * (maxDistance - minDistance) / numPlanets + minDistance;
+			GeneratePlanet (sun.transform, minD, maxD, minPlanetRadius, maxPlanetRadius);
 		}
+	}
+
+	GameObject GeneratePlanet (Transform center, float minD, float maxD, float minR, float maxR) {
+		GameObject planet = Instantiate (planetPrefab, center);
+
+		maxR = (maxD - minD) / 2;
+
+		if (maxR < minR) {
+			Debug.LogWarning ("Minimum radius will produce planet larger than distance range");
+			maxR = minR;
+		}
+
+		float radius = (float)(rand.NextDouble () * (maxR - minR) + minR);
+		planet.transform.localScale *= radius * 2;
+
+		maxD -= radius;
+		minD += radius;
+
+		Orbiter orbiter = planet.GetComponent<Orbiter> ();
+		orbiter.center = center;
+		orbiter.ccw = rand.Next () % 2 == 0;
+		orbiter.distance = (float)(rand.NextDouble () * (maxD - minD) + minD);
+		orbiter.period = orbiter.distance;
+
+		return planet;
 	}
 }
