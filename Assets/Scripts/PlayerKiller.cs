dd969fa69641fa6dfe50dea5aeb5c414a569b621
asampley <sampley@ualberta.ca>
2018-03-17 14:13:55 -0600

    Added player killer script, and put it on planet and sun prefabs

---
+++ Assets/Scripts/PlayerKiller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Collider))]
+public class PlayerKiller : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		Player player = other.GetComponent<Player>();
+		if (player != null) {
+			player.Die();
+		}
+	}
+}
