9023c104bb350f083ccb2d5dfe55be82da610a15
alexczeto <alexczeto@yahoo.ca>
2018-03-17 13:43:30 -0600

    Modified sun and player interations

--- Assets/Scripts/SunBehaviour.cs
+++ Assets/Scripts/SunBehaviour.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SunBehaviour : MonoBehaviour {
-
 	public GameObject Player;
 	// Use this for initialization
 	void Start () {
-		
+		this.Player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 	// Update is called once per frame
 	void Update () {
 		RaycastHit hit;
-		if(Physics.Raycast(transform.position, (Player.transform.position - transform.position), out hit)){
-			if (hit.transform == Player.transform) {
+		if(Physics.Raycast(transform.position, (this.Player.transform.position - transform.position), out hit)){
+			//print (hit.transform);
+			if (hit.transform == this.Player.transform) {
 				print ("Player Hit");
+			} else {
+				print ("Not player");
 			}
 		}
 	}
