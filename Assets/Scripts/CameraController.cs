718f3c6b505fec73005eddac99bfad3e72b974a8
splantio <splant@ualberta.ca>
2018-03-17 13:08:10 -0600

    camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,20 +7,33 @@ public class CameraController : MonoBehaviour {
 
     public GameObject background;
     public Transform player;
-    public float height;
-    public float width;
+    private Vector3 backgroundSize;
+    private float camheight;
+    private float camwidth;
 
 	// Use this for initialization
 	void Start () {
         Camera cam = Camera.main;
-        height = 2f * cam.orthographicSize;
-        width = height * cam.aspect;
+        camheight = 2f * cam.orthographicSize;
+        camwidth = camheight * cam.aspect;
+        backgroundSize = background.GetComponent<MeshRenderer>().bounds.size;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        Constrain();
+    }
+
+    void Constrain()
+    {
         float playerx = player.position.x;
         float playery = player.position.y;
-        transform.position = new Vector3(player.position.x, player.position.y, -5);
-	}
+        float lowerBoundX = (-backgroundSize.x / 2) + (camwidth / 2);
+        float upperBoundX = (backgroundSize.x / 2) - (camwidth / 2);
+        float lowerBoundY = (-backgroundSize.y / 2) + (camheight / 2);
+        float upperBoundY = (backgroundSize.y / 2) - (camheight / 2);
+        float newX = Mathf.Max(lowerBoundX, Mathf.Min(upperBoundX, playerx));
+        float newY = Mathf.Max(lowerBoundY, Mathf.Min(upperBoundY, playery));
+        transform.position = new Vector3(newX, newY, -5);
+    }
 }
