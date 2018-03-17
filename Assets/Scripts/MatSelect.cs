768be2a0d7981ef84145edd7b31d188607221321
alexczeto <alexczeto@yahoo.ca>
2018-03-17 12:46:30 -0600

    Planet Random Skin generator

---
+++ Assets/Scripts/MatSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatSelect : MonoBehaviour {
+	public FileNames fN;
+
+	private Object[] mats;
+	private Material p_Material;
+
+	void Start () {
+		mats = Resources.LoadAll ("planetMat", typeof(Material));
+		Material newMat = (Material)mats[Random.Range(0, mats.Length)];
+		GetComponent<Renderer> ().material = newMat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
