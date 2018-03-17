aa74ffac4773a26af0de5fcfd5c108a45cadff93
alexczeto <alexczeto@yahoo.ca>
2018-03-16 20:40:35 -0600

    Sun script

---
+++ Assets/Scripts/SunBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunBehaviour : MonoBehaviour {
+
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, (Player.transform.position - transform.position), out hit)){
+			if (hit.transform == Player.transform) {
+				print ("Player Hit");
+			}
+		}
+	}
+}
