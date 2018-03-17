620aa959822ca96123b87a4e105fae4ae5b72c25
asampley <sampley@ualberta.ca>
2018-03-17 14:08:11 -0600

    Added game controller to main, and fixed some script issues

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameState : MonoBehaviour {
+public class GameController : MonoBehaviour {
 	public int points = 0;
 	
 	void Reset() {
