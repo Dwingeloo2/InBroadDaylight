b28db1dbe5bbc2792d3097c2bfa1b50c5127099e
asampley <sampley@ualberta.ca>
2018-03-18 14:12:35 -0600

    Added velocity based damage instead of instant killing on player killer script \(instant kill still available\)

--- Assets/Scripts/PlayerKiller.cs
+++ Assets/Scripts/PlayerKiller.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 [RequireComponent (typeof(Collider))]
 public class PlayerKiller : MonoBehaviour {
+	public bool kill = true;
+	public float damagePerSpeed = 0;
+
+	void Awake() {
+		if (!kill && this.GetComponent<Collider> ().isTrigger) {
+			Debug.LogError ("Cannot support damage with velocity for a trigger object");
+		}
+	}
+
 	void OnTriggerEnter(Collider other) {
 		Player player = other.GetComponent<Player>();
 		if (player != null) {
-			player.Die();
+			if (kill) {
+				player.Die ();
+			} else {
+				Debug.LogError ("Cannot support damage with velocity for a trigger object");
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		Player player = other.gameObject.GetComponent<Player>();
+		if (player != null) {
+			if (kill) {
+				player.Die ();
+			} else {
+				player.AffectHealth (-damagePerSpeed * other.relativeVelocity.magnitude);
+			}
 		}
 	}
 }
