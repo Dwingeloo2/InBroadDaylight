51841e7f5b56f8049bbce6c43db9703cc75e0a37
splantio <splant@ualberta.ca>
2018-03-18 17:53:51 -0600

    levels have different lighting

---
+++ Assets/Scripts/LevelLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLight : MonoBehaviour {
+
+    public List<Color> colorsToCycle;
+
+	// Use this for initialization
+	void Start () {
+        int colorIndex = GameController.level % colorsToCycle.Count;
+        gameObject.GetComponent<Light>().color = colorsToCycle[colorIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
