041f4be5b3bf62a43411586c4f3df219345b5cde
asampley <sampley@ualberta.ca>
2018-03-18 15:40:20 -0600

    Changed difficulty by leve

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,6 +14,7 @@ public class GameController : MonoBehaviour {
 
 	void Awake() {
 		generator.seed = level;
+		generator.orbitalPeriodConstant = 10 * Mathf.Pow (0.8, level);
         levelText.GetComponent<Text>().text = "Level  " + level.ToString();
 	}
 
